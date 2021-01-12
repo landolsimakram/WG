b58363b6e01f0ba8e9b83661d8e682da44aebf07
unknown <landolsimakram4@gmail.com>
2021-01-12 07:52:20 +0100

    done

--- login.js
+++ login.js
@@ -1,22 +1,18 @@
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+function moveHands() {
+  with (new Date()) {
+    h = 30 * ((getHours() % 12) + getMinutes() / 60); // 30 degrees hour
+    m = 6 * getMinutes(); // 6 degrees every minute
+    s = 6 * getSeconds(); // 6 degrees every second
+    // setting the rotate CSS attribute to those degree values -->
+    document.getElementById("seconds").style.cssText =
+      "-webkit-transform:rotate(" + s + "deg);";
+    document.getElementById("minutes").style.cssText =
+      "-webkit-transform:rotate(" + m + "deg);";
+    document.getElementById("hours").style.cssText =
+      "-webkit-transform:rotate(" + h + "deg);";
 
-function setDate() {
-  const now = new Date();
-  
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  
-  const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-  
-  const hour = now.getHours();
-  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-};
+    setTimeout(moveHands, 1000); // calling the function every second
+  }
+}
 
-setInterval(setDate, 1000);
-setDate();
+window.onload = moveHands; // making sure the function starts on load of webpage
